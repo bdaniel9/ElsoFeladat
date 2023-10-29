61c9a3ef07884cbe4f14cfab836798f5badc7a5f
Daniel Bochis <bochisdaniel3@gmail.com>
2023-10-29 12:45:39 +0100

    Páros számok

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -6,12 +6,19 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        for (int i = 0;i<10;i++){
-            System.out.println(i+".");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + ".");
         }
 
         Scanner scanner = new Scanner(System.in);
         String nev = scanner.nextLine();
-        System.out.println("Hello "+nev);
+        System.out.println("Add meg a neved:");
+        System.out.println("Hello " + nev);
+
+        for (int x = 1;x<10;x++){
+            if (x%2==0){
+                System.out.println("Páros szám: "+x);
+            }
+        }
     }
 }
